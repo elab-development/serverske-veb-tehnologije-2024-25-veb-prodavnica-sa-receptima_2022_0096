fb74e845a44248dd602d3eca4e58ff402f741134
DusicaLjubicic <dusica03va@gmail.com>
2025-08-15 12:17:39 +0200

    controllers-and-routes-for-authorization

--- recipes/routes/api.php
+++ recipes/routes/api.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/register', [AuthenticationController::class, 'register']);
+Route::post('/login', [AuthenticationController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    
+    Route::post('/logout', [AuthenticationController::class, 'logout']);
 });
